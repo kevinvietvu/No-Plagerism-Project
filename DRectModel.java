527cc3ef064e3bdb97200162d1dff0aa350cde54
stanl33y <shplagata@gmail.com>
2016-05-15 18:15:58 -0700

    made constructors public

--- DRectModel.java
+++ DRectModel.java
@@ -2,11 +2,11 @@ import java.awt.Color;
 
 public class DRectModel extends DShapeModel {
 	
-	DRectModel(int x, int y, int w, int h) {
+	public DRectModel(int x, int y, int w, int h) {
 		super(x, y, w, h);
 	}
 	
-	DRectModel()
+	public DRectModel()
 	{
 		super();
 	}
